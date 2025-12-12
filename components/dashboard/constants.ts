9fd4a4916077599d63e99d706ad87f9e317b62b9
Veer - V <165256694+Veer-V@users.noreply.github.com>
2025-12-12 13:22:36 +0530

    sds

--- components/dashboard/constants.ts
+++ components/dashboard/constants.ts
@@ -30,8 +30,8 @@ export const IMAGES = {
     mono: "../image/monoacting.jpg",
     trade: "../image/trade.jpg",
     debate: "../image/debate.jpg",
-    trashthecan1: "../image/trashthecan.png",
-    mentalhealthquiz1: "../image/mentalhealthquiz.png"
+    trashthecan: "../image/trashthecan.png",
+    mentalhealthquiz: "../image/mentalhealthquiz.png"
   };
 
 export const events: EventData[] = [
